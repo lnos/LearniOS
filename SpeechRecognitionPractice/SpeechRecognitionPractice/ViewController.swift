a909ba118951ced08365ebe422ff8106a876f601
Seong Kim <hoon5421@hotmail.com>
2018-08-19 14:47:44 -0400

    Initial Commit

---
+++ SpeechRecognitionPractice/SpeechRecognitionPractice/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SpeechRecognitionPractice
+//
+//  Created by Seong Kim on 2018-08-19.
+//  Copyright © 2018 Seong Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
