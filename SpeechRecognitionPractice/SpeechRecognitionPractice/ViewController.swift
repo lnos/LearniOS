8432d900c6c2bc9db8e9fe8d5cb654b3e45b114c
Seong Kim <hoon5421@hotmail.com>
2018-08-22 23:30:19 -0400

    Connecting Views to the viewcontroller

--- SpeechRecognitionPractice/SpeechRecognitionPractice/ViewController.swift
+++ SpeechRecognitionPractice/SpeechRecognitionPractice/ViewController.swift
@@ -7,14 +7,25 @@
 //
 
 import UIKit
+import Speech
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var startButton: UIButton!
+    @IBOutlet weak var colorView: UIView!
+    @IBOutlet weak var detectedTextLabel: UILabel!
+    
+    let audioEngine = AVAudioEngine()
+    let speechRecognizer: SFSpeechRecognizer? = SFSpeechRecognizer()
+    let request = SFSpeechAudioBufferRecognitionRequest()
+    var recognitionTask: SFSpeechRecognitionTask?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
