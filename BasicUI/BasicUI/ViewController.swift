6d217eb6bce04f01e40497740beeaaae111b9b4d
Seong Kim <hoon5421@hotmail.com>
2018-09-12 21:10:58 -0400

    Basic UI App

---
+++ BasicUI/BasicUI/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BasicUI
+//
+//  Created by Seong Kim on 2018-09-12.
+//  Copyright © 2018 Seong Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
--- SpeechRecognitionPractice/SpeechRecognitionPractice/ViewController.swift
+++ SpeechRecognitionPractice/SpeechRecognitionPractice/ViewController.swift
@@ -30,6 +30,29 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func recordAndRecognizeSpeech(){
+        let node = audioEngine.inputNode
+        let recordingFormat = node.outputFormat(forBus: 0)
+        node.installTap(onBus: 0, bufferSize: 1024, format: recordingFormat){buffer, _ in
+            self.request.append(buffer)
+        }
+        
+        audioEngine.prepare()
+        do{
+            try audioEngine.start()
+        } catch{
+            return print(error)
+        }
+        
+        guard let myRecognizer = SFSpeechRecognizer() else{
+            return
+        }
+        if !myRecognizer.isAvailable{
+            return
+        }
+        
+    }
 
 
 }
