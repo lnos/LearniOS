d3db7eff79f6cdfebe4317f1c634705efc2e6241
Seong Kim <hoon5421@hotmail.com>
2018-09-16 21:27:49 -0400

    New project with AVAudio

---
+++ Xylophone/Xylophone/ VIewController.swift
@@ -0,0 +1,50 @@
+//
+//  ViewController.swift
+//  Xylophone
+//
+//  Created by Angela Yu on 27/01/2016.
+//  Copyright © 2016 London App Brewery. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController{
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func notePressed(_ sender: UIButton) {
+        playSound(soundButtonTagIndex: sender.tag)
+    }
+    
+    var player: AVAudioPlayer?
+    
+    func playSound(soundButtonTagIndex: Int) {
+        
+        let soundFileName = "note\(soundButtonTagIndex)"
+        guard let url = Bundle.main.url(forResource: soundFileName, withExtension: "wav") else { return }
+        
+        do {
+            try AVAudioSession.sharedInstance().setCategory(AVAudioSessionCategoryPlayback)
+            try AVAudioSession.sharedInstance().setActive(true)
+            
+            /* The following line is required for the player to work on iOS 11. Change the file type accordingly*/
+            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.wav.rawValue)
+            
+            /* iOS 10 and earlier require the following line:
+             player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileTypeMPEGLayer3) */
+            
+            guard let player = player else { return }
+            
+            player.play()
+            
+        } catch let error {
+            print(error.localizedDescription)
+        }
+    }
+
+
+}
+
